97e0cf6438ff0cd5d680538b879dad60d3107f70
mix060514 <mix060514@gmail.com>
2024-11-12 22:42:13 +0800

    docker group add

--- install-docker.sh
+++ install-docker.sh
@@ -17,3 +17,7 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo docker run hello-world
+# sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
