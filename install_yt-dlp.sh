31116487375b4f08fbfe9f6d516bf7ec38197715
mix060514 <mix060514@gmail.com>
2025-05-22 03:29:08 +0800

    Create install_yt-dlp.sh

---
+++ install_yt-dlp.sh
@@ -0,0 +1,5 @@
+sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /usr/local/bin/yt-dlp
+sudo chmod a+rx /usr/local/bin/yt-dlp
+
+sudo apt update
+sudo apt install ffmpeg
