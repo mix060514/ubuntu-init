b953030cec714a0bbf1f3e8dcef79a353cb6355e
mix060514 <mix060514@gmail.com>
2025-05-31 02:12:03 -0700

    Update install_yt-dlp.sh

--- install_yt-dlp.sh
+++ install_yt-dlp.sh
@@ -2,4 +2,4 @@ sudo wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O /u
 sudo chmod a+rx /usr/local/bin/yt-dlp
 
 sudo apt update
-sudo apt install ffmpeg
+sudo apt install ffmpeg -y
